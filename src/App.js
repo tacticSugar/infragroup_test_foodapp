7f492acb28be28b4f75ae1b4680756229e81339c
Vladimir <balaian.vladimir@yandex.ru>
2022-12-20 15:06:42 +0300

    finish header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Fragment } from 'react'
+
+import Header from './components/Layout/Header'
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    <Fragment>
+      <Header />
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
